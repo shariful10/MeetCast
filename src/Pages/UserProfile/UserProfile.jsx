d8e5fd319abb5cbc9f0f85b917087914b61e6dec
Ahmed Nashif <a.nashif7@gmail.com>
2023-08-11 20:51:11 +0600

    fixed

--- src/Pages/UserProfile/UserProfile.jsx
+++ src/Pages/UserProfile/UserProfile.jsx
@@ -12,7 +12,7 @@ const UserProfile = () => {
                 <div className='bg-white p-6 w-3/6 m-auto'>
                     <div className='w-full bg-white flex md:flex-row p-6 border'>
                         <div className='p-6 md:w-1/2'>
-                            <img src={`${user?.photoURL}.jpg`} className='w-full m-auto' alt="" />
+                            <img src={user?.photoURL} className='w-full m-auto' alt="" />
                             <div className='p-6'>
                                 <h3 className='font-bold'>About Me</h3>
                                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A dolores consectetur accusantium nisi. Dignissimos totam provident ad ut impedit non asperiores aliquam quo, consectetur alias fuga possimus perspiciatis facere atque.</p>
