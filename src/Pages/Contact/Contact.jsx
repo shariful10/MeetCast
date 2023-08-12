a9e45b9d001ae8cb6eff32c69d0249f6deaa4a5c
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-08-12 10:50:49 +0600

    Contact Us Page Added

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -25,16 +25,16 @@ const Contact = () => {
 
 	const notify = () => toast("successfully send your email");
 	return (
-		<div className=" bg-gradient-to-t from-[#e5f0fd] to-[rgba(21, 21, 21, 0)]">
+		<div className=" bg-gradient-to-t from-[#e5f0fd] to-[rgba(21, 21, 21, 0)] mt-[96px]">
 			<Container>
 				<div>
 					<h2 className="text-center text-5xl font-bold md:mb-16 mb-8">
 						Get in touch with us
 					</h2>
-					<div className="contact-section bg-indigo-100 md:py-12 md:flex justify-between items-center">
+					<div className="contact-section bg-indigo-100 md:py-12 grid md:grid-cols-2 justify-between gap-6 items-center rounded-2xl py-10">
 						<div className="md:mx-8 mx-4">
 							<div className="md:my-8">
-								<h4 className=" text-xl font-semibold">Drop us a message</h4>
+								<h4 className="text-xl font-semibold">Drop us a message</h4>
 								<p>will you get back to you as soon as possible</p>
 							</div>
 							<form ref={form} onSubmit={sendEmail}>
@@ -75,11 +75,13 @@ const Contact = () => {
 									className="w-full my-2 p-2 rounded-lg"
 									placeholder="Message"
 									name="message"
+									rows="5"
 								/>
 								<br />
 								<input
 									onClick={notify}
-									className="w-full my-2 p-2 bg-black text-white rounded-lg"
+									// className="w-full my-2 p-2 bg-black text-white rounded-lg"
+									className="bg-secondary primary_button w-full py-2 md:py-3 rounded-md hover:rounded-md font-medium md:font-semibold font-NotoSans text-white hover:text-[#1d2130]"
 									type="submit"
 									value="Send"
 								/>
@@ -87,22 +89,38 @@ const Contact = () => {
 							</form>
 						</div>
 
-						<div className="contact-details md:mx-8 py-4">
-							<h4 className="flex items-center md:text-3xl font-bold">
-								<FaPhoneAlt className="bg-indigo-500 text-white w-12 h-12 rounded-full mr-4 p-3"></FaPhoneAlt>{" "}
-								+88 01518-726852
-							</h4>
-							<p className="ml-16">Free support</p>
-							<h4 className="flex items-center md:text-3xl font-bold">
-								<MdOutlineEmail className="bg-indigo-500 text-white w-12 h-12 rounded-full mr-4 p-3"></MdOutlineEmail>
-								skshariful10@gmail.com
-							</h4>
-							<p className="ml-16">Help email support</p>
-							<h4 className="flex items-center md:text-3xl font-bold">
-								<MdOutlineEmail className="bg-indigo-500 text-white w-12 h-12 rounded-full mr-4 p-3"></MdOutlineEmail>
-								antora.taslima97@gmail.com
-							</h4>
-							<p className="ml-16">Seals Enquiry</p>
+						<div className="contact-details mx-4 md:mx-8 py-4">
+							<div className="flex items-center mt-5 md:mt-0">
+								<div className="bg-indigo-500 rounded-full p-3 mr-4">
+									<FaPhoneAlt className="text-white w-6 h-6"></FaPhoneAlt>
+								</div>{" "}
+								<div className="">
+									<span className="md:text-2xl font-bold">+88 01518-726852</span>
+									<p>Free support</p>
+								</div>
+							</div>
+							<div className="flex items-center my-5">
+								<div className="bg-indigo-500 rounded-full p-3 mr-4">
+									<MdOutlineEmail className="text-white w-6 h-6"></MdOutlineEmail>
+								</div>{" "}
+								<div className="">
+									<span className="md:text-2xl font-bold">
+										skshariful10@gmail.com
+									</span>
+									<p>Help email support</p>
+								</div>
+							</div>
+							<div className="flex items-center">
+								<div className="bg-indigo-500 rounded-full p-3 mr-4">
+									<MdOutlineEmail className="text-white w-6 h-6"></MdOutlineEmail>
+								</div>{" "}
+								<div className="">
+									<span className="md:text-2xl font-bold">
+										antora.taslima97@gmail.com
+									</span>
+									<p>Seals Enquiry</p>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
