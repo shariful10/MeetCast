1564ebeac614e37d01f005ed7d7adff75f0b5cad
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-08-20 12:20:26 +0600

    Features Section Added

--- src/Pages/Home/Features/Features.jsx
+++ src/Pages/Home/Features/Features.jsx
@@ -1,21 +1,103 @@
 import React from "react";
 import { GiVideoConference } from "react-icons/gi";
+import { GoDeviceCameraVideo, GoGear } from "react-icons/go";
+import { HiOutlineUserGroup } from "react-icons/hi";
+import { BsChatLeftDots } from "react-icons/bs";
+import Container from "../../../Components/Shared/Container/Container";
 
 const Features = () => {
 	return (
-		<div>
-			<div className="">
-				<div className="bg-blue-400 w-10 h-10 rounded-full flex justify-center items-center text-white">
-					<GiVideoConference className="h-6 w-6 p-5" />
+		<div className="bg-[#F0F2FE] py-16">
+			<Container>
+				<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+					<div>
+						<div className="bg-blue-400 w-14 h-14 rounded-2xl flex justify-center items-center text-white">
+							<GiVideoConference size={25} />
+						</div>
+						<h3 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold my-5">
+							Video Webinar
+						</h3>
+						<p>
+							Enhance your website with video webinars. Connect globally, interact in
+							real-time, share engaging content, and build your brand. Perfect for
+							presentations, training, and knowledge sharing. Elevate your online
+							presence with video webinars.
+						</p>
+					</div>
+					<div>
+						<div className="bg-yellow-500 w-14 h-14 rounded-2xl flex justify-center items-center text-white">
+							<GoDeviceCameraVideo size={25} />
+						</div>
+						<h3 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold my-5">
+							Meetings
+						</h3>
+						<p>
+							Enhance your website with video webinars. Connect globally, interact in
+							real-time, share engaging content, and build your brand. Perfect for
+							presentations, training, and knowledge sharing. Elevate your online
+							presence with video webinars.
+						</p>
+					</div>
+					<div>
+						<div className="bg-blue-700 w-14 h-14 rounded-2xl flex justify-center items-center text-white">
+							<HiOutlineUserGroup size={25} />
+						</div>
+						<h3 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold my-5">
+							Video Group
+						</h3>
+						<p>
+							Enhance your website with video webinars. Connect globally, interact in
+							real-time, share engaging content, and build your brand. Perfect for
+							presentations, training, and knowledge sharing. Elevate your online
+							presence with video webinars.
+						</p>
+					</div>
 				</div>
-				<h3>Video Webinar</h3>
-				<p>
-					Enhance your website with video webinars. Connect globally, interact in
-					real-time, share engaging content, and build your brand. Perfect for
-					presentations, training, and knowledge sharing. Elevate your online presence
-					with video webinars.
-				</p>
-			</div>
+				<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 mt-16">
+					<div>
+						<div className="bg-red-600 w-14 h-14 rounded-2xl flex justify-center items-center text-white">
+							<HiOutlineUserGroup size={25} />
+						</div>
+						<h3 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold my-5">
+							Conference
+						</h3>
+						<p>
+							Enhance your website with video webinars. Connect globally, interact in
+							real-time, share engaging content, and build your brand. Perfect for
+							presentations, training, and knowledge sharing. Elevate your online
+							presence with video webinars.
+						</p>
+					</div>
+					<div>
+						<div className="bg-fuchsia-500 w-14 h-14 rounded-2xl flex justify-center items-center text-white">
+							<BsChatLeftDots size={25} />
+						</div>
+						<h3 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold my-5">
+							Chat
+						</h3>
+						<p>
+							Enhance your website with video webinars. Connect globally, interact in
+							real-time, share engaging content, and build your brand. Perfect for
+							presentations, training, and knowledge sharing. Elevate your online
+							presence with video webinars.
+						</p>
+					</div>
+					<div>
+						<div className="bg-green-400 w-14 h-14 rounded-2xl flex justify-center items-center text-white">
+							<GoGear size={25} />
+						</div>
+						<h3 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold my-5">
+							Online Webinar
+						</h3>
+						<p>
+							Enhance your website with video webinars. Connect globally, interact in
+							real-time, share engaging content, and build your brand. Perfect for
+							presentations, training, and knowledge sharing. Elevate your online
+							presence with video webinars.
+						</p>
+					</div>
+				</div>
+			</Container>
 		</div>
 	);
 };
