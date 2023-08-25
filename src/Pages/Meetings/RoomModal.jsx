0b0d9b64680a3837d6ea40c8e88d40d453fb11be
Sulthanmahmud Suvo <sulthansuvo72@gmailcom>
2023-08-25 17:25:07 +0600

    update modal

--- src/Pages/Meetings/RoomModal.jsx
+++ src/Pages/Meetings/RoomModal.jsx
@@ -86,7 +86,7 @@ const RoomModal = ({ show, onHide, room, onRename }) => {
                   </div>
                 </div>
               </div>
-              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+              <div className="bg-gray-50 px-3 py-2 sm:px-6 sm:flex sm:flex-row-reverse">
                 <button
                   type="button"
                   className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
