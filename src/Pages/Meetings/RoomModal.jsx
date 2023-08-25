d8b4d058d9979464ad026b31dfba0d37e2e78472
Sulthanmahmud Suvo <sulthansuvo72@gmailcom>
2023-08-25 17:09:34 +0600

    update modal

--- src/Pages/Meetings/RoomModal.jsx
+++ src/Pages/Meetings/RoomModal.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
 import { toast } from "react-hot-toast";
 
 const RoomModal = ({ show, onHide, room, onRename }) => {
@@ -15,36 +16,97 @@ const RoomModal = ({ show, onHide, room, onRename }) => {
   };
 
   return (
-    <div
-      className={`fixed inset-0 flex items-center justify-center transition-opacity duration-300 ${
-        show ? "opacity-100" : "opacity-0 pointer-events-none"
-      }`}
-    >
-      <div className="bg-white p-4 rounded-lg shadow-md">
-        <h2 className="text-lg font-semibold mb-2">Rename Room</h2>
-        <input
-          type="text"
-          className="w-full border p-2 mb-2"
-          placeholder="New Room Name"
-          value={newRoomName}
-          onChange={(e) => setNewRoomName(e.target.value)}
-        />
-        <div className="flex justify-end">
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded mr-2"
-            onClick={handleRename}
+    <Transition.Root show={show} as={Fragment}>
+      <Dialog
+        as="div"
+        static
+        className="fixed inset-0 overflow-y-auto z-50"
+        open={show}
+        onClose={onHide}
+      >
+        <div className="flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:p-0">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            Rename
-          </button>
-          <button
-            className="bg-gray-300 text-gray-700 px-4 py-2 rounded"
-            onClick={onHide}
+            <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-50 transition-opacity" />
+          </Transition.Child>
+
+          <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+            &#8203;
+          </span>
+
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
           >
-            Cancel
-          </button>
+            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
+                    {/* You can replace this with an icon or text */}
+                    <svg
+                      className="h-6 w-6 text-blue-600"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h16m-7 6h7"
+                      />
+                    </svg>
+                  </div>
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                      Rename Room
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <input
+                        type="text"
+                        className="w-full border p-2 mb-2"
+                        placeholder="New Room Name"
+                        value={newRoomName}
+                        onChange={(e) => setNewRoomName(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <button
+                  type="button"
+                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+                  onClick={handleRename}
+                >
+                  Rename
+                </button>
+                <button
+                  type="button"
+                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:w-auto sm:text-sm"
+                  onClick={onHide}
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </Transition.Child>
         </div>
-      </div>
-    </div>
+      </Dialog>
+    </Transition.Root>
   );
 };
 
