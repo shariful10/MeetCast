41959ce7f1b9c988c7c5f19436343a1716eead71
Sulthanmahmud Suvo <sulthansuvo72@gmailcom>
2023-08-29 10:57:27 +0600

    update

--- src/Pages/Meetings/Meetings.jsx
+++ src/Pages/Meetings/Meetings.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from "react";
 import Messaging from "./Messaging";
 import Users from "./Users";
-import RoomSection from "./RoomSection";
-import VideoMeet from "./VideoMeet";
-
 const Meetings = () => {
   const [roomNumber, setRoomNumber] = useState()
-  const gettingRoomNumber =(getRoomId)=>{
+  const gettingRoomNumber = (getRoomId) => {
     const room = getRoomId
     setRoomNumber(room)
   }
@@ -17,7 +14,7 @@ const Meetings = () => {
   return (
     <div className="mt-28 border m-2 p-2">
       <div className="flex">
-        <Users gettingRoomNumber={gettingRoomNumber}></Users> 
+        <Users gettingRoomNumber={gettingRoomNumber}></Users>
         <Messaging room={roomNumber}></Messaging>
       </div>
     </div>
