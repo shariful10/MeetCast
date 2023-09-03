8956ea27b84b0a9920240411f83ad2f24bc20b49
Ahmed Nashif <a.nashif7@gmail.com>
2023-09-03 10:24:30 +0600

    trying to fix

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -10,54 +10,75 @@ import UserProfile from "../Pages/UserProfile/UserProfile";
 import PrivateRoute from "./PrivateRoute";
 import MeetingSchedule from "./../Pages/MeetingSchedule/MeetingSchedule";
 import Meetings from "../Pages/Meetings/Meetings";
-import EditUserProfile from "../Pages/UserProfile/EditUserProfile";
 import DashboardLayout from "../Layouts/DashboardLayout/DashboardLayout";
 import MyMeetings from "../Pages/MyMeetings/MyMeetings";
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Main />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/pricing",
-        element: <Pricing />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-      {
-        path: "/schedule",
-        element: <MeetingSchedule />,
-      },
-      {
-        path: "/userProfile",
-        element: <PrivateRoute>
-          <UserProfile />,
-        </PrivateRoute>
-      },
-      {
-        path: "/meetings",
-        element: <PrivateRoute>
-          <Meetings></Meetings>
-        </PrivateRoute>
-      },
-    ],
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signup",
-    element: <SignUp />,
-  },
+	{
+		path: "/",
+		element: <Main />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/about",
+				element: <About />,
+			},
+			{
+				path: "/pricing",
+				element: <Pricing />,
+			},
+			{
+				path: "/contact",
+				element: <Contact />,
+			},
+			// {
+			// 	path: "/userProfile",
+			// 	element: (
+			// 		<PrivateRoute>
+			// 			<UserProfile />,
+			// 		</PrivateRoute>
+			// 	),
+			// },
+			{
+				path: "/meetings",
+				element: (
+					<PrivateRoute>
+						<Meetings></Meetings>
+					</PrivateRoute>
+				),
+			},
+		],
+	},
+	{
+		path: "/login",
+		element: <Login />,
+	},
+	{
+		path: "/signup",
+		element: <SignUp />,
+	},
+	{
+		path: "/dashboard",
+		element: (
+			<PrivateRoute>
+				<DashboardLayout />
+			</PrivateRoute>
+		),
+		children: [
+			{
+				path: "/dashboard/userProfile",
+				element: <UserProfile />,
+			},
+			{
+				path: "/dashboard/schedule",
+				element: <MeetingSchedule />,
+			},
+			{
+				path: "/dashboard/myMeetings",
+				element: <MyMeetings />,
+			},
+		],
+	},
 ]);
