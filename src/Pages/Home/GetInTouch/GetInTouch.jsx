9653fb0b34044842b92c1a8f1b9afe9f04538795
Wixden <watchonlytutorial@gmail.com>
2023-09-08 20:33:54 +0600

    consult section added

--- src/Pages/Home/GetInTouch/GetInTouch.jsx
+++ src/Pages/Home/GetInTouch/GetInTouch.jsx
@@ -1,41 +1,84 @@
 import React from "react";
 import Container from "../../../Components/Shared/Container/Container";
 import { Link } from "react-router-dom";
+import VerticalLine from "../../../Components/VerticalLine/VerticalLine";
 
 const GetInTouch = () => {
-	return (
-		<div className="bg-darkBg min-h-[450px] text-white flex justify-center items-center py-20 md:py-8">
-			<Container>
-				<div className="md:flex justify-around md:gap-20 items-center py-5 space-y-20 md:space-y-0">
-					<div className="max-w-[490px]">
-						<p className="text-sm font-bold uppercase">Why Choose Us ?</p>
-						<h1 className="text-3xl md:text-5xl font-extrabold font-manrope capitalize mt-4 mb-8">
-							Bridging Gaps Through Cutting-Edge Conferencing
-						</h1>
-						<p className="md:text-lg  font-medium">
-							Experience crystal-clear communication and content sharing on our
-							innovative video conferencing platform.
-						</p>
-					</div>
-					<div className="max-w-[400px] w-full">
-						<input
-							type="text"
-							placeholder="Email Address"
-							className="bg-[#F2F3F4] bg-opacity-20 p-4 rounded-md w-full mb-10"
-						/>
-						<input
-							type="password"
-							placeholder="Password"
-							className="bg-[#F2F3F4] bg-opacity-20 p-4 rounded-md w-full mb-10"
-						/>
-						<button className="bg-secondary px-5 md:px-10 py-4 rounded md:font-semibold font-NotoSans get_started_button text-white hover:rounded-md w-full">
-							<Link to="/login">GET STARTED</Link>
-						</button>
-					</div>
-				</div>
-			</Container>
-		</div>
-	);
+  return (
+    <div
+      className="relative  min-h-[450px] bg-cover bg-center text-white bg-fixed scroll-smooth"
+      style={{
+        backgroundImage:
+          "url('https://images.unsplash.com/photo-1573166364839-1bfe9196c23e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80')",
+      }}
+    >
+      <div className="flex justify-center items-center py-20 md:py-8 bg-darkBg bg-opacity-40">
+        <Container>
+          <div className="md:flex justify-around md:gap-20 items-center py-5 space-y-20 md:space-y-0">
+            <div className="max-w-[490px]">
+              <p
+                className="text-sm font-bold uppercase"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="200"
+              >
+                Why Choose Us ?
+              </p>
+              <VerticalLine
+                color="bg-darkBg"
+                height="h-1"
+                width="w-16"
+                marginY="my-3"
+              />
+              <h1
+                className="text-3xl md:text-5xl font-extrabold font-manrope capitalize mt-4 mb-8"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="600"
+              >
+                Bridging Gaps Through Cutting-Edge Conferencing
+              </h1>
+              <p
+                className="md:text-lg  font-medium"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="1000"
+              >
+                Experience crystal-clear communication and content sharing on
+                our innovative video conferencing platform.
+              </p>
+            </div>
+            <div className="max-w-[400px] w-full">
+              <input
+                type="text"
+                placeholder="Email Address"
+                className="bg-[#F2F3F4] bg-opacity-20 p-4 rounded-md w-full mb-10"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="600"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                className="bg-[#F2F3F4] bg-opacity-20 p-4 rounded-md w-full mb-10"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="1000"
+              />
+              <button
+                className="bg-secondary px-5 md:px-10 py-4 rounded md:font-semibold font-NotoSans get_started_button text-white hover:rounded-md w-full"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="1400"
+              >
+                <Link to="/login">GET STARTED</Link>
+              </button>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
 };
 
 export default GetInTouch;
--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -4,66 +4,66 @@ import VerticalLine from "../../Components/VerticalLine/VerticalLine";
 import heroImage from "../../assets/hero-image.png";
 
 const HeroSection = () => {
-	return (
-		<div
-			className="bg-white md:h-[800px] bg-contain bg-no-repeat w-full md:flex justify-center items-center py-32 md:py-20"
-			style={{ backgroundImage: `url(${heroBg})` }}
-		>
-			<Container>
-				<div className="grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-5 align-middle">
-					<div>
-						<div className="md:max-w-[545px] lg:max-w-[600px] xl:max-w-[700px] order-1">
-							<p className="text-sm md:text-xl my-3 font-semibold capitalize">
-								More connected. more collaborative, more intelligent
-							</p>
-							<VerticalLine
-								color="bg-secondary"
-								height="h-1"
-								width="w-16"
-								marginY="my-3"
-							/>
-							<h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold ">
-								Powering organizations across industries and geographies
-							</h1>
-							<p
-								className="text-gray-400 my-5 text-xs md:text-lg"
-								data-aos="fade-right"
-								data-aos-duration="1200"
-								data-aos-delay="800"
-							>
-								Securely connect and collaborate so you can work better together.
-								Simple to manage and delightful to use, MeetCast powers the modern
-								workforce.
-							</p>
-							<div className="py-8 flex items-center gap-8">
-								<div
-									data-aos="fade-up"
-									data-aos-duration="1200"
-									data-aos-delay="1100"
-								>
-									<button className="bg-secondary w-[150px] py-3 md:py-4 rounded-full font-medium md:font-semibold font-NotoSans primary_button text-white">
-										SignUp Now
-									</button>
-								</div>
-								<div
-									data-aos="fade-up"
-									data-aos-duration="1200"
-									data-aos-delay="1400"
-								>
-									<button className="bg-primary w-[150px] py-3 md:py-4 rounded-full font-medium md:font-semibold font-NotoSans primary_button text-white">
-										Explore
-									</button>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="order-first md:order-last">
-						<img src={heroImage} alt="Hero Image" />
-					</div>
-				</div>
-			</Container>
-		</div>
-	);
+  return (
+    <div
+      className="bg-white md:h-[800px] bg-contain bg-no-repeat w-full md:flex justify-center items-center py-32 md:py-20"
+      style={{ backgroundImage: `url(${heroBg})` }}
+    >
+      <Container>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-5 items-center">
+          <div>
+            <div className="md:max-w-[545px] lg:max-w-[600px] xl:max-w-[700px] order-1">
+              <p className="text-sm md:text-xl my-3 font-semibold capitalize">
+                More connected. more collaborative, more intelligent
+              </p>
+              <VerticalLine
+                color="bg-secondary"
+                height="h-1"
+                width="w-16"
+                marginY="my-3"
+              />
+              <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold ">
+                Powering organizations across industries and geographies
+              </h1>
+              <p
+                className="text-gray-400 my-5 text-xs md:text-lg"
+                data-aos="fade-right"
+                data-aos-duration="1200"
+                data-aos-delay="800"
+              >
+                Securely connect and collaborate so you can work better
+                together. Simple to manage and delightful to use, MeetCast
+                powers the modern workforce.
+              </p>
+              <div className="py-8 flex items-center gap-8">
+                <div
+                  data-aos="fade-up"
+                  data-aos-duration="1200"
+                  data-aos-delay="1100"
+                >
+                  <button className="bg-secondary w-[150px] py-3 md:py-4 rounded-full font-medium md:font-semibold font-NotoSans primary_button text-white">
+                    SignUp Now
+                  </button>
+                </div>
+                <div
+                  data-aos="fade-up"
+                  data-aos-duration="1200"
+                  data-aos-delay="1400"
+                >
+                  <button className="bg-primary w-[150px] py-3 md:py-4 rounded-full font-medium md:font-semibold font-NotoSans primary_button text-white">
+                    Explore
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="order-first md:order-last">
+            <img src={heroImage} alt="Hero Image" />
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default HeroSection;
