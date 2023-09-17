125aee8386f634225a61702fc5bd9800057abb34
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-09-17 12:45:05 +0600

    Something

--- src/Pages/AdminHome/UserNumberCard.jsx
+++ src/Pages/AdminHome/UserNumberCard.jsx
@@ -22,7 +22,7 @@ const UserNumberCard = () => {
     <div className="w-[300px] m-auto bg-slate-100 hover:bg-slate-200 rounded-lg shadow-lg p-6 text-center flex flex-grow">
       <div className="w-5/6">
         <h3 className="text-start text-2xl font-bold">USERS</h3>
-        <h2 className="text-start text-4xl">{allUsers?.length || "00"}</h2>
+        <h2 className="text-start text-4xl">{allUsers?.length ? allUsers?.length : "00"}</h2>
         <h2 className="text-start text-sm mt-6"><span className="text-green-600 me-3"><FontAwesomeIcon icon={faArrowUp}/> 10%</span>Increse this month</h2>
       </div>
       <div className="flex justify-center text-2xl">
