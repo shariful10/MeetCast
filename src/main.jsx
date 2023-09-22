a3bf690982d5a133f4f6062bcec817bf23672ba8
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-09-22 20:29:40 +0600

    Update Something rbg

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import AuthProvider from "./Providers/AuthProvider";
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { router } from "./Routes/Routes";
-import CustomChatBot from "./Components/Shared/CustomChatBot/CustomChatBot";
 
 const queryClient = new QueryClient();
 
@@ -31,7 +30,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
             },
           }}
         />
-        <CustomChatBot />
         <RouterProvider router={router} />
       </AuthProvider>
     </QueryClientProvider>
