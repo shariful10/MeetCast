d0d4a96d2c0d094eeaf8357e210aed6f9034625b
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-03 17:39:35 +0600

    Updated Something

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,8 +3,8 @@ import DashboardLayout from "../Layouts/DashboardLayout/DashboardLayout";
 import Main from "../Layouts/Main/Main";
 import About from "../Pages/About/About";
 import AddBlog from "../Pages/AddBlog/AddBlog";
-import MyBlog from './../Pages/Dashboard/MyBlog';
-import AdminHome from './../Pages/AdminHome/AdminHome';
+import MyBlog from "./../Pages/Dashboard/MyBlog";
+import AdminHome from "./../Pages/AdminHome/AdminHome";
 import BlogPage from "../Pages/Blog/BlogPage";
 import BlogPost from "../Pages/Blog/BlogPost";
 import CheckOut from "../Pages/CheckOut/CheckOut";
@@ -13,21 +13,28 @@ import EditorHome from "../Pages/EditorHome/EditorHome";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
-import ChatRoom from "../Pages/Meetings/ChatRoom";
 import MyMeetings from "../Pages/MyMeetings/MyMeetings";
 import PaymentFaild from "../Pages/PaymentFaild/PaymentFaild";
 import PaymentSucc from "../Pages/PaymentSucc/PaymentSucc";
 import Pricing from "../Pages/Pricing/Pricing";
-import RoomPage from "../Pages/RoomPage/RoomPage";
 import SignUp from "../Pages/SignUp/SignUp";
 import UserHome from "../Pages/UserHome/UserHome";
-import EditUserProfile from "../Pages/UserProfile/EditUserProfile";
-import UserProfile from "../Pages/UserProfile/UserProfile";
 import MeetingSchedule from "./../Pages/MeetingSchedule/MeetingSchedule";
 import UserManagement from "./../Pages/UserManagement/UserManagement";
 import AdminRoute from "./AdminRoute";
 import EditorRoute from "./EditorRoute";
 import PrivateRoute from "./PrivateRoute";
+import ManageBlogs from "../Pages/ManageBlogs/ManageBlogs";
+import DisplayInfo from "../Pages/UserProfile/DisplayInfo";
+import PersonalInfo from "../Pages/UserProfile/PersonalInfo";
+import MeetingInfo from "../Pages/UserProfile/MeetingInfo";
+import ChatBox from "../Pages/ChatRoom/ChatBox";
+import UserProfileDashboardLayout from "../Layouts/UserProfileDashboardLayout/UserProfileDashboardLayout";
+import UserPrivacySettings from "../Pages/UserProfile/UserPrivacySettings";
+import UserBillingAndSubscription from "../Pages/UserProfile/UserBillingAndSubscription";
+import UserAvatar from "../Pages/UserProfile/UserAvatar";
+import UpdateBlog from "../Pages/UpdateBlog/UpdateBlog";
+import UserRoute from "./UserRoute";
 export const router = createBrowserRouter([
 	{
 		path: "/",
@@ -54,6 +61,7 @@ export const router = createBrowserRouter([
 				path: "/blog/:id",
 				element: <BlogPost />,
 			},
+
 			{
 				path: "/contact",
 				element: <Contact />,
@@ -68,16 +76,6 @@ export const router = createBrowserRouter([
 				loader: ({ params }) =>
 					fetch(`${import.meta.env.VITE_API_URL}/checkout/${params.id}`),
 			},
-
-
-			{
-				path: "/meetings",
-				element: (
-					<PrivateRoute>
-						<ChatRoom></ChatRoom>
-					</PrivateRoute>
-				),
-			},
 		],
 	},
 	{
@@ -90,15 +88,27 @@ export const router = createBrowserRouter([
 	},
 	{
 		path: "/payment/success/:tranId",
-		element: <PaymentSucc></PaymentSucc>,
+		element: (
+			<PrivateRoute>
+				<PaymentSucc />
+			</PrivateRoute>
+		),
 	},
 	{
 		path: "/payment/faild/:tranId",
-		element: <PaymentFaild></PaymentFaild>,
+		element: (
+			<PrivateRoute>
+				<PaymentFaild />
+			</PrivateRoute>
+		),
 	},
 	{
-		path: "/room/:roomID",
-		element: <RoomPage />,
+		path: "/meetings",
+		element: (
+			<PrivateRoute>
+				<ChatBox />
+			</PrivateRoute>
+		),
 	},
 	{
 		path: "/dashboard",
@@ -107,6 +117,7 @@ export const router = createBrowserRouter([
 				<DashboardLayout />
 			</PrivateRoute>
 		),
+		errorElement: <ErrorPage />,
 		children: [
 			{
 				path: "/dashboard/admin-home",
@@ -120,45 +131,132 @@ export const router = createBrowserRouter([
 				path: "/dashboard/editor-home",
 				element: (
 					<EditorRoute>
-						<UserHome />
+						<EditorHome />
 					</EditorRoute>
 				),
 			},
 			{
-				path: "/dashboard/add-blog",
-				element: <AddBlog />,
+				path: "/dashboard/userHome",
+				element: (
+					<UserRoute>
+						<UserHome />
+					</UserRoute>
+				),
 			},
 			{
-				path: "/dashboard/adminUserProfile",
-				element: <EditUserProfile />,
+				path: "/dashboard/add-blog",
+				element: (
+					<EditorRoute>
+						<AddBlog />
+					</EditorRoute>
+				),
+			},
+			{
+				path: "/dashboard/update-blog/:id",
+				element: (
+					<EditorRoute>
+						<UpdateBlog />
+					</EditorRoute>
+				),
+				loader: ({ params }) => fetch(`${import.meta.env.VITE_API_URL}/blog/${params.id}`),
 			},
 			{
 				path: "/dashboard/userProfile",
-				element: <UserProfile />,
-			},
-			{
-				path: "/dashboard/userHome",
-				element: <UserHome />,
-			},
-			{
-				path: "/dashboard/editUserProfile",
-				element: <EditUserProfile />,
+				element: (
+					<PrivateRoute>
+						<UserProfileDashboardLayout />
+					</PrivateRoute>
+				),
+				children: [
+					{
+						path: "/dashboard/userProfile/editPhoto",
+						element: (
+							<PrivateRoute>
+								<UserAvatar />
+							</PrivateRoute>
+						),
+					},
+					{
+						path: "/dashboard/userProfile/",
+						element: (
+							<PrivateRoute>
+								<DisplayInfo />
+							</PrivateRoute>
+						),
+					},
+					{
+						path: "/dashboard/userProfile/personalInfo",
+						element: (
+							<PrivateRoute>
+								<PersonalInfo />
+							</PrivateRoute>
+						),
+					},
+					{
+						path: "/dashboard/userProfile/meetingInfo",
+						element: (
+							<PrivateRoute>
+								<MeetingInfo />
+							</PrivateRoute>
+						),
+					},
+					{
+						path: "/dashboard/userProfile/billing",
+						element: (
+							<PrivateRoute>
+								<UserBillingAndSubscription />
+							</PrivateRoute>
+						),
+					},
+					{
+						path: "/dashboard/userProfile/privacy",
+						element: (
+							<PrivateRoute>
+								<UserPrivacySettings />
+							</PrivateRoute>
+						),
+					},
+				],
 			},
 			{
 				path: "/dashboard/schedule",
-				element: <MeetingSchedule />,
+				element: (
+					<PrivateRoute>
+						<MeetingSchedule />
+					</PrivateRoute>
+				),
 			},
 			{
-				path: "/dashboard/myMeetings",
-				element: <MyMeetings />,
+				path: "/dashboard/my-meetings",
+				element: (
+					<PrivateRoute>
+						<MyMeetings />
+					</PrivateRoute>
+				),
 			},
 			{
 				path: "/dashboard/UserManagement",
-				element: <UserManagement />,
+				element: (
+					<AdminRoute>
+						<UserManagement />
+					</AdminRoute>
+				),
 			},
 			{
 				path: "/dashboard/my-blog",
-				element: <MyBlog />,
+				element: (
+					<EditorRoute>
+						<MyBlog />
+					</EditorRoute>
+				),
+			},
+			{
+				path: "/dashboard/manage-blogs",
+				element: (
+					<AdminRoute>
+						<ManageBlogs />
+					</AdminRoute>
+				),
 			},
 		],
 	},
